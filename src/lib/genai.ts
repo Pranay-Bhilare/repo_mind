7bca255fb5bcb8eef80a233336dd0130ea593ba5
bhilare_ <bhilarespranay81@gmail.com>
2025-04-25 02:12:03 +0530

    Implemented summary embeddings genration & storage

--- src/lib/genai.ts
+++ src/lib/genai.ts
@@ -1,7 +1,8 @@
 import {GoogleGenerativeAI} from "@google/generative-ai"
+import type { Document } from "@langchain/core/documents";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API!)
-const model = genAI.getGenerativeModel({model : "gemini-2.0-flash-lite"})
+const model = genAI.getGenerativeModel({model : "gemini-1.5-flash"})
 
 export const aiSummariseCommit = async (diff: string) => {
     try {
@@ -43,4 +44,32 @@ export const aiSummariseCommit = async (diff: string) => {
         console.error("Error generating commit summary:", error);
         return "Failed to generate summary due to an error.";
     }
+}
+
+export const summariseCode =  async (doc: Document) => {
+    const code = doc.pageContent.slice(0,10000)  // Limit to 10k characters
+    try {
+        const response = await model.generateContent([
+            `You are an intelligent senior software engineer who specializes in onboarding junior software engineers onto projects.
+            You are onboarding a junior software engineer and explaining to them the purpose of the ${doc.metadata.source} file
+            
+            Here is the code 
+            ---
+            ${code}
+            ---
+            
+            Give me a summary no more than 100 words of the code provided above`
+        ]);
+        return response.response.text();
+    } catch (error){
+        console.error("Error generating code summary:", error);
+        return "Failed to generate summary due to an error.";
+    }
+}
+
+export const generateEmbedding = async (text: string) => {
+    const model = genAI.getGenerativeModel({model : "text-embedding-004"});
+    const result = await model.embedContent(text);
+    const embeddings =  result.embedding
+    return embeddings.values;
 }
--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,4 +1,7 @@
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import type { Document } from "@langchain/core/documents";
+import { generateEmbedding, summariseCode } from "./genai";
+import { db } from "@/server/db";
 
 export const loadGithubRepo = async (githubUrl: string, githubToken? : string) => {
     const loader = new GithubRepoLoader(githubUrl, {
@@ -11,4 +14,39 @@ export const loadGithubRepo = async (githubUrl: string, githubToken? : string) =
     });
     const docs = await loader.load();
     return docs;
+}
+
+export const indexGithubRepo = async (projectId :string,githubUrl: string, githubToken? : string) => {
+    const docs = await loadGithubRepo(githubUrl, githubToken);
+    const allEmbeddings = await generateEmbeddings(docs);
+    await Promise.allSettled(allEmbeddings.map(async (embedding,index) => {
+        console.log(`processing ${index} of ${allEmbeddings.length}`)
+        if(!embedding) return;
+        const sourceCodeEmbedding = await db.sourceCodeEmbeddings.create({
+            data : {
+                summary : embedding.summary,
+                sourceCode : embedding.sourceCode,
+                fileName : embedding.fileName,
+                projectId : projectId
+            }
+        })
+
+        await db.$executeRaw`
+            UPDATE "SourceCodeEmbeddings"
+            SET "summaryEmbedding" = ${embedding.embeddings}::vector
+            WHERE "id" = ${sourceCodeEmbedding.id}
+         `
+    }));
+}
+const generateEmbeddings = async(docs : Document[]) => {
+    return await Promise.all(docs.map(async doc => {
+        const summary = await summariseCode(doc);
+        const embeddings =  await generateEmbedding(summary);
+        return {
+            summary,
+            embeddings,
+            sourceCode : JSON.parse(JSON.stringify(doc.pageContent)),
+            fileName : doc.metadata.source
+        }
+    }))
 }
